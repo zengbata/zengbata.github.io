82fe4db24e5eccb8443532846fc6860a5de4396f
zengbata <436032407@qq.com>
2016-05-18 10:40:11 +0800

    脚本练习

---
+++ 12/object.js
@@ -0,0 +1,18 @@
+var students = {
+    name:"abc",
+    age:20,
+    major:"广告学"
+    sayHello:function(){
+    	console.log("你好!")
+    }
+
+console.log(studen.name);
+console.log(student.major)
+student.sayHello();
+
+for (var i = student) {
+	Things[i]
+}
+}
+}
+
